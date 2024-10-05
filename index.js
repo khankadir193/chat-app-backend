2a25608b065e1c4dd1ad7c1fa7862dee408a4714
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-10-05 20:40:12 +0530

    route added

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+const PORT = process.env.PORT || 5000;
+
+// Handle socket connections
+io.on('connection', (socket) => {
+  console.log('New client connected');
+
+  // Receiving and broadcasting messages
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
